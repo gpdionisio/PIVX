63ec19e16b351bd29ea6a4a612c288ae47374c1b
practicalswift <practicalswift@users.noreply.github.com>
2017-08-09 16:14:37 +0200

    Use unique_ptr for pfilter (CBloomFilter)

--- src/net.cpp
+++ src/net.cpp
@@ -2334,7 +2334,7 @@ CNode::CNode(NodeId idIn, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn
     nNextLocalAddrSend = 0;
     nNextAddrSend = 0;
     fRelayTxes = false;
-    pfilter = new CBloomFilter();
+    pfilter = std::unique_ptr<CBloomFilter>(new CBloomFilter());
     timeLastMempoolReq = 0;
     nPingNonceSent = 0;
     nPingUsecStart = 0;
@@ -2358,8 +2358,6 @@ CNode::CNode(NodeId idIn, ServiceFlags nLocalServicesIn, int nMyStartingHeightIn
 CNode::~CNode()
 {
     CloseSocket(hSocket);
-
-    delete pfilter;
 }
 
 void CNode::AskFor(const CInv& inv)
